9c5e2a59cdabec742966d7e88bd2c35e2be1d5fd
Vitalii <116552240+sharaievPn@users.noreply.github.com>
2023-04-04 15:37:33 +0300

    Update app.py
    english and ukrainian support

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify, session
 from flask_session import Session
-from script import create_map
+import script_eng
+import script_ukr
 
 
 app = Flask(__name__)
@@ -9,10 +10,11 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
+@app.route('/uk/', methods=['GET', 'POST'])
+@app.route('/uk', methods=['GET', 'POST'])
+def home_ukrainian():
     if request.method == 'GET':
-        return render_template('main.html')
+        return render_template('main_ukr.html')
     else:
         data = request.get_json()
         lat = data['latitude']
@@ -22,21 +24,78 @@ def home():
         return jsonify({'success': True})
 
 
-@app.route('/map', methods=['GET', 'POST'])
-def introduce():
+@app.route('/uk/map', methods=['GET', 'POST'])
+@app.route('/uk/map/', methods=['GET', 'POST'])
+def introduce_ukrainian():
     if request.method == 'GET':
         lat = session.get('lat')
         lng = session.get('lng')
-        create_map(lat, lng)
-        return render_template('map.html')
+        script_ukr.create_map(lat, lng)
+        return render_template('map_ukr.html')
     else:
         return 'yo'
 
-#
-#
-# @app.route('/no_perm')
-# def introduce2():
-#     return render_template('main_no_perm.html')
+
+@app.route('/en/')
+@app.route('/en')
+def home_english():
+    if request.method == 'GET':
+        return render_template('main_eng.html')
+    else:
+        data = request.get_json()
+        lat = data['latitude']
+        lng = data['longitude']
+        session['lat'] = lat
+        session['lng'] = lng
+        return jsonify({'success': True})
+
+
+@app.route('/en/map')
+@app.route('/en/map/')
+def introduce_english():
+    if request.method == 'GET':
+        lat = session.get('lat')
+        lng = session.get('lng')
+        script_eng.create_map(lat, lng)
+        return render_template('map_eng.html')
+    else:
+        return 'yo'
+
+
+@app.route('/uk/about-us/', method=['GET'])
+@app.route('/uk/about-us', method=['GET'])
+def about_us_ukrainian():
+    pass
+
+
+@app.route('/en/about-us/', method=['GET'])
+@app.route('/en/about-us', method=['GET'])
+def about_us_english():
+    pass
+
+
+@app.route('/uk/policies/', method=['GET'])
+@app.route('/uk/policies', method=['GET'])
+def policies_ukrainian():
+    pass
+
+
+@app.route('/en/policies/', method=['GET'])
+@app.route('/en/policies', method=['GET'])
+def policies_english():
+    pass
+
+
+@app.route('/uk/error/', method=['GET'])
+@app.route('/uk/error', method=['GET'])
+def error_ukrainian():
+    pass
+
+
+@app.route('/en/error/', method=['GET'])
+@app.route('/en/error', method=['GET'])
+def error_english():
+    pass
 
 
 if __name__ == '__main__':
